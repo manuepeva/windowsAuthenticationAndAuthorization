4bfcf13940f1451349e816a1150f02a9d0b3d14a
Manuel <manuepeva@gmail.com>
2017-12-10 10:57:15 -0500

    merging

---
+++ WebApplication2/Controllers/EmployeesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication2.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<EMPLOYEESTABLE> Get()
+        {
+            using(mydbEntities1 entities = new mydbEntities1())
+            {
+                return entities.EMPLOYEESTABLE.ToList();
+            }
+        }
+    }
+}
